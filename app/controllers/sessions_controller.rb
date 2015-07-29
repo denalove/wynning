9d5e1b7a9532bf267ba2f31a96030a736aac0301
Dena Sawyer <denasawyer1111@gmail.com>
2015-07-29 19:46:32 -0400

    logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+    def create
+  	@user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+
+  	if @user
+  		session[:user_id] = @user.id
+  		redirect_to user_path
+  	else
+  		render action: 'new'
+  	end
+  end
 end
+
