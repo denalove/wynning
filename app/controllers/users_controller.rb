3b7c45cdac257a6d149745c8fa5569517475c033
Dena Sawyer <denasawyer1111@gmail.com>
2015-07-29 19:23:58 -0400

    actually save the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def index
     render :new
   end
+
+  def create
+  	@user = User.new user_params
+
+  	if @user.save
+  		redirect_to root_path, notice: "Created user"
+  	else
+  		render action: 'new'
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+  
 end
