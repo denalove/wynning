589673f6ea36c7a22f0e845ba9608663521eb6f1
Anthony Tamayo <anthonyatamayo@gmail.com>
2015-07-30 17:15:05 -0400

    wrote test for high level goal

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :one_month_goals
   has_secure_password
   validates :email, uniqueness: true
+  validates :high_level_goal, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,12 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email: "ed@wyncode.co", password: "anything")
     assert_not user.save, "Hey, dummy...same email."
   end
+
+  test "high level goal should not be nil" do
+    user = users(:one)
+    user.high_level_goal = nil
+    assert_not user.save, "you need a goal in life, dummy"
+    p user.errors.messages
+  end
+
 end
