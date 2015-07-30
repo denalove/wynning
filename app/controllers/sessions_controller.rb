e551c313e6939667e0fae7348c64ba33d79bdea6
Jonathan Rose <jonr87ee@gmail.com>
2015-07-30 16:31:40 -0400

    add login and logout links. Update signup to redirect to profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,7 @@ class SessionsController < ApplicationController
 
   def create
   	@user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
-
-  	if @user && @user.authenticate(params[:session][:password])
+  	if @user
   		session[:user_id] = @user.id
   		redirect_to @user
   	else
@@ -14,8 +13,9 @@ class SessionsController < ApplicationController
   	end
   end
 
+
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to root_path
   end
 end
