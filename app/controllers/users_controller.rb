397c31ae93d3115c5e9d11ff9e2228060eb0863f
Jonathan Rose <jonr87ee@gmail.com>
2015-07-30 10:45:46 -0400

    update sign up process. add index view for users for debugging. update user show view to display high level goal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  # def index
-  # 	@users = User.all 
-  #   render :new
-  # end
 
   def show
+    @user = current_user
+  end
+
+  def index
+    @users = User.all
   end
 
 
@@ -15,6 +17,7 @@ class UsersController < ApplicationController
   	@user = User.new user_params
 
   	if @user.save
+      session[:user_id] = @user.id
   		redirect_to root_path, notice: "Created user"
   	else
   		render action: 'new'
@@ -24,7 +27,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  	params.require(:user).permit(:username, :password, :password_confirmation)
+  	params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :last_name, :high_level_goal, :email, :phone_number)
   end
 
 end
