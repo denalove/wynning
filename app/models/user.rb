1068265809ec74bf4e6ec2ba71a4a780cfe80832
Dena Sawyer <denasawyer1111@gmail.com>
2015-08-02 13:34:09 -0400

    update validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
 	has_many :one_month_goals
 	has_secure_password
-	validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "Email is not valid" }
+	validates :email, uniqueness: true, 
+										format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 	validates :high_level_goal, presence: true
-	validates :username, presence: true, length: { minimum: 5}
-	validates :username, uniqueness: true
+	validates :username, presence: true, 
+											 length: { minimum: 5 },
+											 uniqueness: true
 end
