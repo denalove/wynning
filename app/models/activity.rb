94f4a5cb6ae9deb73902a509474371ac330ae6bb
Jonathan Rose <jonr87ee@gmail.com>
2015-08-01 22:53:38 -0400

    add activity completion logging to activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,24 @@
 class Activity < ActiveRecord::Base
   belongs_to :one_month_goal
+
+
+  def daily_complete
+    @completed =[]
+    day = self.start.yday
+    today = Time.new.yday
+    diff = day - today
+    @completed[diff] = true
+    @completed
+  end
+
+  def score
+    @score=0
+    @completed.each do |check|
+      @score +=1 if check
+    end
+    @score *= 100
+    @score /= 7
+    @score
+  end
+
 end
