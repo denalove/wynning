63f223cbc181df6384ef1fbb38d405a2b99493e0
Jonathan Rose <jonr87ee@gmail.com>
2015-08-02 18:30:06 -0400

    Fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 	has_many :one_month_goals
 	has_secure_password
 	#validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-	#validates :high_level_goal, presence: true
+	validates :high_level_goal, presence: true
 	validates :username, presence: true, length: { minimum: 5 }, uniqueness: true
 end
