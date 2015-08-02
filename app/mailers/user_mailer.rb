642a0617b973ce1c9ba698abefcbaec09bf733aa
Dena Sawyer <denasawyer1111@gmail.com>
2015-08-02 17:39:32 -0400

    capitalize names

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 	def welcome_email(user)
 		@user = user
 		@url = 'http://wynning.co/Login'
-		email_with_name = %("#{@user.first_name}" <#{@user.email} > )
+		email_with_name = %("#{@user.first_name.capitalize}" <#{@user.email} > )
 		mail(to: email_with_name, subject: 'Welcome to Wynning!')
 	end
 
