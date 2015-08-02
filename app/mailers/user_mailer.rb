dcca363f738079ab938aca989e731292d10211d2
Dena Sawyer <denasawyer1111@gmail.com>
2015-08-02 19:58:25 -0400

    Add letter opener gem and make mailer work with gmail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
 	def welcome_email(user)
 		@user = user
-		@url = 'http://wynning.co/Login'
+		@url = 'http://wynning.co'
 		email_with_name = %("#{@user.first_name.capitalize}" <#{@user.email} > )
 		mail(to: email_with_name, subject: 'Welcome to Wynning!')
 	end
