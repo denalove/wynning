66e70edcb0ec745279daf4ad77dac86344503195
Dena Sawyer <denasawyer1111@gmail.com>
2015-08-05 18:24:26 -0400

    Make high-level-goals model and controller

--- app/models/high_level.rb
+++ app/models/high_level.rb
@@ -1,4 +1,5 @@
 class HighLevel < ActiveRecord::Base
   belongs_to :user
+  has_many :one_months_goals
   
 end
--- app/models/one_month_goal.rb
+++ app/models/one_month_goal.rb
@@ -1,4 +1,6 @@
 class OneMonthGoal < ActiveRecord::Base
   belongs_to :user
+  belongs_to :high_level
   has_many :activities
+  
 end
