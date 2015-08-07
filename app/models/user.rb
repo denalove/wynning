afaa7edfef02969110e0a9aa3c0847d636f69cb7
Anthony Tamayo <anthonyatamayo@gmail.com>
2015-08-06 22:24:04 -0400

    restored changes to landing page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	has_many :one_month_goals
 	has_many :high_levels
 	has_secure_password
-	#validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+	validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 	validates :high_levels, presence: true
 	validates :username, presence: true, length: { minimum: 5 }, uniqueness: true
 	accepts_nested_attributes_for :high_levels
