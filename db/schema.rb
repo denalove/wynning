910a5642207f141651bb42dd9d5f8f97f8f79bad
Jonathan Rose <jonr87ee@gmail.com>
2015-08-06 20:27:29 -0400

    Fix due date in activites db issue. Migration clean up

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20150805155923) do
     t.string   "summary"
     t.date     "last"
     t.integer  "score"
-    t.date     "due_due"
+    t.date     "due_date"
     t.integer  "one_month_goal_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
