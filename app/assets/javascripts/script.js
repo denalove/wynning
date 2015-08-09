f02af8afe55ae841a8c06f7e9d7b791fddd6fe35
Jonathan Rose <jonr87ee@gmail.com>
2015-08-09 15:32:10 -0400

    small fixes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,7 +2,7 @@ var template = function(text) {
   return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
 };
 
-// var main = function() {
+var main = function() {
 //
 //   // Adds new item on submit
 //   $('form').on('submit', function() {
